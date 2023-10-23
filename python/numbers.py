2a6cf5d7af20a6d5ba56b20fdabebcaa021f593e
michaelorina <orinamichael4@gmail.com>
2023-10-23 15:52:21 +0300

    lesson 11 numbers

---
+++ python/numbers.py
@@ -0,0 +1,14 @@
+num1 = 100
+num2 = 15
+num3 = 10.55
+
+print(num1 + num2)
+print(num3 - num1)
+print(num2 * num3)
+print(num1 / num2)
+print(1 + 10 / 5 - 6 * 2)
+print(10 ** 3.25)
+print(10 % 3)
+print(100265638274 % 17)
+print(10 // 3)
+print(191 // 7)
