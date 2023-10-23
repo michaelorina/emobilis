952d41b796983bd315b6d16205988fdd3c3bdd9b
michaelorina <orinamichael4@gmail.com>
2023-10-23 15:27:16 +0300

    lesson 11 variables

---
+++ python/variables.py
@@ -0,0 +1,17 @@
+x = 10
+y = 16
+pi = 22/7
+radius = 13.5
+name = "Michael"
+city = "Nairobi"
+sentence = "The quick brown fox jumped over a lazy dog"
+distance_to_moyale = 750
+person_no_1 = "Jane"
+max_loan_amount = 10000
+is_raining = False
+is_complete = True
+PI = 3.142
+MAX_WITHDRAWAL_AMOUNT = 60000
+
+print(name, city, max_loan_amount)
+print(pi)
