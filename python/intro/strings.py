86de9716e2c2b4c7886f56a6a16f8d3442a11359
michaelorina <orinamichael4@gmail.com>
2023-10-23 16:07:18 +0300

    lesson 11 strings

---
+++ python/intro/strings.py
@@ -0,0 +1,10 @@
+name = "Orinah"
+city = "Cairo"
+age = 22
+weight = 70.8
+
+print(name + city)
+print(name * 4)
+# print(name + age)
+
+print(f"I am {name}, I am {age} years old and I live in {city}. I weigh {weight} Kgs")
