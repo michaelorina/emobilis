d95b2aaafde48a8d04d749ac98a630de3cdb1dbb
michaelorina <orinamichael4@gmail.com>
2023-10-24 15:40:38 +0300

    lesson 12 py_collections

---
+++ python/intro/py_collections.py
@@ -0,0 +1,17 @@
+# lists, dictionary, set, tuple
+
+cities = ["Kigoma", "Kigali", "Kinshasa", "Kisumu", "Dodoma", "Nairobi", "Bujumbura"]
+scores = [56, 45, 79, 89, 94, 90, 67]
+
+car = {
+    "make": "Toyota",
+    "model": "Probox",
+    "year": 2017,
+    "gear": "Manual",
+    "color": "White",
+    "owners": ["Jane", "Jerry", "Miry"]
+}
+
+friends = {"Mike", "Walid", "Ahmed", "John", "Mike", "Walid"} #unique elements
+
+workers = ("Jane", "Jack", "Jim") #never change it programmatically
