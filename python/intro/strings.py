168a4f8136b866b334c73f3c39bdc8533ece84a0
michaelorina <orinamichael4@gmail.com>
2023-10-24 14:57:27 +0300

    lesson 12 string functions

--- python/intro/strings.py
+++ python/intro/strings.py
@@ -8,3 +8,16 @@ print(name * 4)
 # print(name + age)
 
 print(f"I am {name}, I am {age} years old and I live in {city}. I weigh {weight} Kgs")
+
+sentence = "These are python functions to do with strings"
+
+upper_case = sentence.upper()
+
+print(upper_case)
+print(sentence.upper())
+print(upper_case.lower())
+print(len(sentence))
+print(sentence.capitalize())
+print(name.swapcase())
+print(sentence)
+print(sentence.strip())
