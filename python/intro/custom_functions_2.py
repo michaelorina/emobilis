f346dae8fa39197980702fcf0e31d39d159a6929
michaelorina <orinamichael4@gmail.com>
2023-10-25 15:30:24 +0300

    lesson 13 custom functions 2

---
+++ python/intro/custom_functions_2.py
@@ -0,0 +1,6 @@
+from intro.custom_functions import say_my_name, add_numbers
+from intro.custom_functions import calculate_volume_cylinder as vol_cylinder
+
+say_my_name("Michael")
+add_numbers(10, 9, 75)
+vol_cylinder(10, 20)
