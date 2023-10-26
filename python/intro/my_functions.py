ff9f8bdcac0e292f378d78cd42770d8c816da44f
michaelorina <orinamichael4@gmail.com>
2023-10-26 15:30:22 +0300

    lesson 14 external imports

---
+++ python/intro/my_functions.py
@@ -0,0 +1,6 @@
+def add(a, b):
+    print(a + b)
+
+
+def multiply(a, b, c):
+    print(a * b * c)
---
+++ python/intro/use.py
@@ -0,0 +1,13 @@
+# import my_functions
+#
+# my_functions.multiply(2, 3, 5)
+# my_functions.add(76, 4)
+
+from my_functions import add, multiply
+
+from my_functions import multiply as times
+
+add(23, 89)
+multiply(8, 3, 2)
+
+times(5, 9, 2)
