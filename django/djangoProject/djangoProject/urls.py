447407b7a80f53a897ed186126436dc4135826df
michaelorina <orinamichael4@gmail.com>
2023-11-01 15:00:48 +0300

    lesson 18 Welcome to Django!

--- django/djangoProject/djangoProject/urls.py
+++ django/djangoProject/djangoProject/urls.py
@@ -17,6 +17,13 @@ Including another URLconf
 # from django.contrib import admin
 from django.urls import path
 
+from mainapp import views
+
 urlpatterns = [
+    path('', views.index),
+    path('about', views.about),
+    path('services', views.services),
+    path('team', views.team),
+    path('contact', views.contact),
     #    path('admin/', admin.site.urls),
 ]
--- django/djangoProject/mainapp/views.py
+++ django/djangoProject/mainapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def services(request):
+    return render(request, 'services.html')
+
+
+def team(request):
+    return render(request, 'team.html')
+
+
+def contact(request):
+    return render(request, 'team.html')
