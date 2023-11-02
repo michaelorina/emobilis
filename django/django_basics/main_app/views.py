9800adc581469e968b55f209e673d67d1e10c4d0
michaelorina <orinamichael4@gmail.com>
2023-11-02 15:55:18 +0300

    lesson 19 django basics

--- django/django_basics/main_app/views.py
+++ django/django_basics/main_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from main_app.users import people
+
+
 # Create your views here.
+def home_page(request):
+    name = "Michael Orina"
+    age = 18
+    users = people
+    data = {
+        "name": name,
+        "age": age,
+        "users": people
+    }
+    return render(request, "index.html", context=data)
