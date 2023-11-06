77a17ab4422c35ef4b00622d5df2f6a0994a7ec6
michaelorina <orinamichael4@gmail.com>
2023-11-06 22:55:53 +0300

    lesson 21 data project

--- django/data_project/data_project/urls.py
+++ django/data_project/data_project/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from main_app import views
+
 urlpatterns = [
+    path('', views.home, name="home"),
     path('admin/', admin.site.urls),
 ]
 
