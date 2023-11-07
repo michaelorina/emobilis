57fc4ed418de3df4fdf7f2883d8dff0c4fa87f11
michaelorina <orinamichael4@gmail.com>
2023-11-07 11:02:09 +0300

    lesson 22 data project

--- django/data_project/main_app/models.py
+++ django/data_project/main_app/models.py
@@ -4,12 +4,15 @@ from django.db import models
 # Create your models here.
 
 class Employee(models.Model):
-    # name, email, dob, salary, didabled
+    # name, email, dob, salary, disabled
     name = models.CharField(max_length=40)
     email = models.EmailField(unique=True)
     dob = models.DateField(null=True)
     salary = models.DecimalField(max_digits=7, decimal_places=2)
     disabled = models.BooleanField(default=False)
+    profile = models.ImageField(upload_to="employees", null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return self.name
--- django/data_project/main_app/views.py
+++ django/data_project/main_app/views.py
@@ -6,10 +6,11 @@ from main_app.app_forms import EmployeeForm
 # Create your views here.
 def home(request):
     if request.method == "POST":
-        form = EmployeeForm(request.POST)
+        form = EmployeeForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect("home")
 
-    form = EmployeeForm()
+    else:
+        form = EmployeeForm()
     return render(request, "employee.html", {"form": form})
