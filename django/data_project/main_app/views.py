5b1bf7390bc3a9f8d487fb5698c427b451e7ac1f
michaelorina <orinamichael4@gmail.com>
2023-11-07 11:46:05 +0300

    lesson 22 data project

--- django/data_project/main_app/views.py
+++ django/data_project/main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 
 from main_app.app_forms import EmployeeForm
+from main_app.models import Employee
 
 
 # Create your views here.
@@ -14,3 +15,13 @@ def home(request):
     else:
         form = EmployeeForm()
     return render(request, "employee.html", {"form": form})
+
+
+def all_employees(request):
+    employees = Employee.objects.all()
+    return render(request, "all_employees.html", {"employees": employees})
+
+
+def employee_details(request, emp_id):
+    employee = Employee.objects.get(pk=emp_id)
+    return render(request, "employee_details.html", {"employee": employee})
