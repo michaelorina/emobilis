251ad20c710d4934a1a4abbc5bcda14e9c240602
michaelorina <orinamichael4@gmail.com>
2023-11-08 13:58:44 +0300

    lesson 23 data project

--- django/data_project/main_app/management/commands/populate.py
+++ django/data_project/main_app/management/commands/populate.py
@@ -1,8 +1,30 @@
+import json
+import os.path
+
 from django.core.management import BaseCommand
+
+from data_project import settings
+from main_app.models import Employee
 
 
 class Command(BaseCommand):
     help = " Populate employees table with 1000 records "
 
     def handle(self, *args, **options):
-        pass
+        path = os.path.join(settings.BASE_DIR, "employees.json")
+        self.stdout.write(
+            self.style.SUCCESS("Started to import data")
+        )
+        with open(path) as file:
+            employees = json.load(file)
+            for e in employees:
+                Employee.objects.create(
+                    name=e['name'],
+                    email=e['email'],
+                    dob=e['dob'],
+                    salary=e['salary'],
+                    disabled=e['disabled'],
+                )
+        self.stdout.write(
+            self.style.SUCCESS("Completed importing data")
+        )
