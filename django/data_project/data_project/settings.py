608d2129e0048fffef3fa34113c8f46caee494ad
michaelorina <orinamichael4@gmail.com>
2023-11-09 10:38:33 +0300

    lesson 24 data project

--- django/data_project/data_project/settings.py
+++ django/data_project/data_project/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x36qzw7kfe)2gi0q_xl%kheyr0^)u8!ei96g_6!hrhh816f4c3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
