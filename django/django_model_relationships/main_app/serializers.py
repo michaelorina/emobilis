c7f9b3a26e62f990050d2faf984933f236d1dfa8
michaelorina <orinamichael4@gmail.com>
2023-11-16 13:39:48 +0300

    lesson 28 api

--- django/django_model_relationships/main_app/serializers.py
+++ django/django_model_relationships/main_app/serializers.py
@@ -12,7 +12,7 @@ class ArtistSerializer(serializers.ModelSerializer):
 class AlbumSerializer(serializers.ModelSerializer):
     class Meta:
         model = Album
-        fields = ['name', 'release_year', 'artists', 'id']
+        fields = ['name', 'release_year', 'id']
 
 
 class SongSerializer(serializers.ModelSerializer):
@@ -22,6 +22,7 @@ class SongSerializer(serializers.ModelSerializer):
 
 
 class ArtistAlbumSerializer(serializers.ModelSerializer):
+    albums = serializers.StringRelatedField(read_only=True, many=True)
     class Meta:
         model = Artist
         fields = ["name", "phone", "albums"]
